7650f292dd6b0ae00240be022978bdf88ab281d4
John Lange <lange.john.m@gmail.com>
2017-11-14 22:53:39 -0500

    Add auto-migrate to global.asax on server

--- Server/LauncherServer/Global.asax.cs
+++ Server/LauncherServer/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -18,6 +20,20 @@ namespace LauncherServer
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Configure DB updates to occur if the web.config says it is ok
+            if (bool.Parse(ConfigurationManager.AppSettings["MigrateDatabaseToLatestVersion"]))
+            {
+                var configuration = new LauncherServer.Migrations.LauncherDbContext.Configuration();
+                var migrator = new DbMigrator(configuration);
+                migrator.Update();
+
+                var configuration_application = new LauncherServer.Migrations.ApplicationDbContext.Configuration();
+                migrator = new DbMigrator(configuration_application);
+                migrator.Update();
+
+
+            }
         }
     }
 }
